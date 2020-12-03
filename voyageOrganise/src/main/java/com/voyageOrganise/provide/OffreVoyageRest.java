c122f83ef1a0544893e102d50f01a41a92b82558
fatimahafid <fatima.hafid00@gmail.com>
2020-12-03 19:48:38 +0100

    commit1

--- voyageOrganise/src/main/java/com/voyageOrganise/provide/OffreVoyageRest.java
+++ voyageOrganise/src/main/java/com/voyageOrganise/provide/OffreVoyageRest.java
@@ -1,5 +1,11 @@
 package com.voyageOrganise.provide;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("api/OffreVoyage")
 public class OffreVoyageRest {
 
 }
